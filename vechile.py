e90899fea110320c81942414e08466e232418e67
vishalsingh0357 <singhvishal9368@gmail.in>
2025-07-18 13:17:21 +0530

    adding libraries

--- vechile.py
+++ vechile.py
@@ -3,6 +3,11 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import LabelEncoder
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import RandomizedSearchCV
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 df = pd.read_csv("vechile.csv")
 print(df.info())
 
